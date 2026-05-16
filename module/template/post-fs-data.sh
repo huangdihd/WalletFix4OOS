e8cb23065b9fbf6c6e621c42923496bd865c9428
huangdihd <hd20100104@163.com>
2026-05-16 20:32:00 +0800

    feat: add region spoofing properties to post-fs-data.sh

--- module/template/post-fs-data.sh
+++ module/template/post-fs-data.sh
@@ -1,1 +1,6 @@
 MODDIR=${0%/*}
+
+# Reset region properties to CN
+resetprop -n ro.oplus.image.my_region.type CN_all
+resetprop -n ro.oplus.pipeline.region CN
+resetprop -n ro.vendor.oplus.regionmark CN
